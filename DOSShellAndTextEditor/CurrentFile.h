ae44501051c38241f5ca9ad7d75e3de47e87959a
amnaamir605@gmail.com <amnaamir605@gmail.com>
2023-11-21 23:23:52 +0500

    Document save and load

--- DOSShellAndTextEditor/CurrentFile.h
+++ DOSShellAndTextEditor/CurrentFile.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<list>
 #include<iostream>
+#include<fstream>
 #include"Lines.h"
 #include"utility.h"
 using namespace std;
@@ -11,13 +12,14 @@ class CurrentFile
 	list<Lines>::iterator ri;
 	int Curr_row, Curr_col;
 	list<char>::iterator ci;
-
+	string Name;
  public:
 
 	 CurrentFile();
 	 void Insert();
 	 void Set_Max_and_Count(Lines& L);
 	 void Print();
-
+	 void SaveFile();
+	 void LoadFile();
 };
 
